a5b1f82a93f77c59d0ed33dde817f15fbff4e96e
salamkhn <motivation3272@gmail.com>
2025-04-27 23:48:36 -0700

    speechRecognization

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useState } from "react"
+import "./Style.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+const App = () => {
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+
+
+  // START LISTINGIN
+  const listening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-Pk' })
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
+  // FUNCTION FOR THE HANDLING COPY
+  const handleCopy = () => {
+    navigator.clipboard.writeText(transcript)
+    alert("Text is Copied")
+    SpeechRecognition.resetTranscript();
+  }
+  return (<>
+    <div className="main-container">
+      <h1 className="heading">Speech to Text Converter</h1>
+      <div className="main-div">
+        <p>{transcript}</p>
+      </div>
+      <div className="btns">
+        <button onClick={handleCopy} className="copy">
+          Copy
+        </button>
+        <button onClick={SpeechRecognition.stopListening} className="stop">
+          Stop listening
+        </button>
+        <button onClick={listening} className="start">
+          Start Listening
+        </button>
+      </div>
+    </div>
+
+  </>)
+
+}
+export default App
